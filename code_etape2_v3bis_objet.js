72d36c16610bd5f13cf56efa83b9584e01f03cc5
alicegdev <71830893+alicegdev@users.noreply.github.com>
2021-02-09 11:25:38 +0100

    Add files via upload
    - Méthode d'instance créée

--- code_etape2_v3bis_objet.js
+++ code_etape2_v3bis_objet.js
@@ -6,6 +6,12 @@ class Generateur {
         this.deuxiemeBoutDePhrase = deuxiemeBoutDePhrase;
         this.troisiemeBoutDePhrase = troisiemeBoutDePhrase;
     }
+    combinaisons(){
+    let randomPremierBoutDePhrase = this.premierBoutDePhrase[Math.floor(Math.random() * this.premierBoutDePhrase.length)];
+    let randomDeuxiemeBoutDePhrase= this.deuxiemeBoutDePhrase[Math.floor(Math.random() * this.deuxiemeBoutDePhrase.length)];
+    let randomTroisiemeBoutDePhrase = this.troisiemeBoutDePhrase[Math.floor(Math.random() * this.troisiemeBoutDePhrase.length)];
+    return randomPremierBoutDePhrase + randomDeuxiemeBoutDePhrase + randomTroisiemeBoutDePhrase;
+    }
 }
 
 let proverbesConfuceens = new Generateur (["La sagesse du sage ", "La lueur des étoiles ", "La patience de la tortue ", "La fureur du tigre "], ["rend plus sage ", "transforme en colline ", "change en prince ", "sommeille dans "], ["même les plus fous.", "le plus vil des crapeaux.", "la plus haute montagne.", "le doute le plus abyssal."]);
@@ -13,25 +19,18 @@ let proverbesConfuceens = new Generateur (["La sagesse du sage ", "La lueur des 
 let burgerQuiz = new Generateur (["Qui est le plus vieux :  ", "Questionnaire de rapidité : ", "Qu'est-ce qui est le plus bruyant : ", "Qui est le plus fort en maths :  "], ["Yoda ou ", "Albator ou ", "vos voisins ou ", "une perceuse ou "], ["Louis XIV ? ", "Didier, le chien joué par Alain Chabat ?", "la SNCF ?", "Matt Pokora ?"]
   );
   
-  function combinaisons(Generateur) {
-    var randomPremierBoutDePhrase = Generateur.premierBoutDePhrase[Math.floor(Math.random() * Generateur.premierBoutDePhrase.length)];
-    var randomDeuxiemeBoutDePhrase= Generateur.deuxiemeBoutDePhrase[Math.floor(Math.random() * Generateur.deuxiemeBoutDePhrase.length)];
-    var randomTroisiemeBoutDePhrase = Generateur.troisiemeBoutDePhrase[Math.floor(Math.random() * Generateur.troisiemeBoutDePhrase.length)];
-    return randomPremierBoutDePhrase + randomDeuxiemeBoutDePhrase + randomTroisiemeBoutDePhrase;
-  }
-  // Créer les combinaisons de phrases sous forme de fonction
-  
+  // Créer la boîte de dialogue
   function commencer() {
       let fois = prompt("Combien de citations ou de questions souhaitez-vous voir ?");
-      var choix = prompt("Quelles citations souhaitez-vous lire ? Pour les faux proverbes confucéens, tapez 1. Pour les questions du Burger Quiz, tapez 2.");
+      let choix = prompt("Quelles citations souhaitez-vous lire ? Pour les faux proverbes confucéens, tapez 1. Pour les questions du Burger Quiz, tapez 2.");
       for (let i = 0; i<fois; i++) {
           switch(choix) {
               case "1":
-                  alert(combinaisons(proverbesConfuceens));
+                  alert(proverbesConfuceens.combinaisons());
                   break;
   
               case "2":
-                  alert(combinaisons(burgerQuiz));
+                  alert(burgerQuiz.combinaisons());
                   break;
   
               default:
@@ -42,15 +41,14 @@ let burgerQuiz = new Generateur (["Qui est le plus vieux :  ", "Questionnaire de
   
   function demandeRepetitions() {
       let onRepart = confirm("Souhaitez-vous voir d'autres citations ? Si oui, cliquez sur OK. Si non, cliquez sur Annuler.");
-      if (onRepart =true) {
+      if (onRepart == true) {
           commencer();
       } else {
           return;
-       //Redemander une fois à la fin avec une récursivité ?
       }
   }
-  
-  // Créer la première boîte de dialogue
+  // Attention aux égalités simples dans les if
+  // Passer les fonctions en méthodes de classes
   
   commencer();
   demandeRepetitions();
